2ff613809e8a7865483ecaf67b209a56b2ddb6c4
atgczcl <atgczcl@163.com>
2015-09-30 11:20:42 +0800

    Add it

---
+++ JTInvokeTask.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 重复定时器默认无限重复
+/// </summary>
+public class JTInvokeTask : MonoBehaviour {
+    float time = 1;
+    float delayTime;
+    System.Action callback;
+    bool isRepeat;
+    int repeatCount = -1;
+
+    /// <summary>
+    /// 延迟定时调用
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="isBeginNow"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    public static JTInvokeTask InitOnce(float time, bool isBeginNow, System.Action callback)
+    {
+        return InitRepeat(time, 0, false, 1, isBeginNow, callback);
+    }
+
+    /// <summary>
+    /// 指定重复次数的调用
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="repeadDelayTime"></param>
+    /// <param name="repeatCount"></param>
+    /// <param name="isBeginNow"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    public static JTInvokeTask InitCount(float time, float repeadDelayTime, int repeatCount, bool isBeginNow, System.Action callback)
+    {
+        return InitRepeat(time, repeadDelayTime, true, repeatCount, isBeginNow, callback);
+    }
+
+    /// <summary>
+    /// repeatCount = -1 无限重复
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="repeadDelayTime"></param>
+    /// <param name="isRepeat"></param>
+    /// <param name="repeatCount"></param>
+    /// <param name="isBeginNow"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    public static JTInvokeTask InitRepeat(float time, float repeadDelayTime, bool isRepeat, int repeatCount, bool isBeginNow, System.Action callback)
+    {
+
+        if (repeatCount == 0)
+        {
+            Debug.LogError("repeat count is 0! can't begin!");
+            return null;
+        }
+
+        GameObject go = new GameObject("JTInvokeTask");
+        JTInvokeTask task = go.AddComponent<JTInvokeTask>();
+        task.callback = callback;
+        task.time = time;
+        task.delayTime = repeadDelayTime;
+        task.isRepeat = isRepeat;
+        task.repeatCount = repeatCount;
+        if (isBeginNow) task.OnBegin();
+        return task;
+    }
+
+    public void begin() {
+        OnBegin();
+    }
+
+    void OnBegin() {
+        if (IsInvoking("OnInvoke")) CancelInvoke("OnInvoke");
+
+        if (isRepeat)
+        {
+            InvokeRepeating("OnInvoke", delayTime, time);
+        }
+        else {
+            Invoke("OnInvoke", time);
+        }
+    }
+
+    void OnInvoke() {
+        if (callback != null) {
+            callback();
+        }
+        if (repeatCount >= 0) { 
+            repeatCount--;
+            if (repeatCount <= 0) Clear();
+        }
+    }
+
+    /// <summary>
+    /// 暂停，之后可以继续使用
+    /// </summary>
+    public void Stop() {
+        CancelInvoke("OnInvoke");
+    }
+
+    /// <summary>
+    /// 直接清除，不能再次使用
+    /// </summary>
+    public void Clear() {
+        Destroy(gameObject);
+    }
+}
---
+++ JTTimeTask.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 整数型时间计数器
+/// </summary>
+public class JTTimeTask : MonoBehaviour {
+    float runFloatTime;
+    int runIntTime;
+    System.Action<int> callback;
+    bool isStop;
+    protected int step = 1;
+
+    /*///////////////////////test///////////////////////
+    JTTimeTask task;
+    public void Awake()
+    {
+        task = JTTimeTask.Init(OnTime, 3);
+        task.start();
+    }
+
+    void OnTime(int time)
+    {
+        Debug.LogError(time);
+    }
+    ///////////////////////////////////////////////*/
+
+    /// <summary>
+    /// 步长默认为step = 1时间的计时器，一秒下发一次
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public static JTTimeTask Init(System.Action<int> time) {
+        return Init(time, 1);
+    }
+
+    /// <summary>
+    /// 步长为step时间的计时器，step秒下发一次
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="step"></param>
+    /// <returns></returns>
+    public static JTTimeTask Init(System.Action<int> time, int step)
+    {
+        if (step <= 0) {
+            Debug.LogError("need step > 0");
+            return null;
+        }
+        GameObject go = new GameObject("TimeTask");
+        JTTimeTask task = go.AddComponent<JTTimeTask>();
+        task.onBegin(time);
+        task.step = step;
+        return task;
+    }
+
+    public void begin() {
+        runFloatTime = 0;
+        runIntTime = 0;
+        flag = -1;
+        isStop = false;
+    }
+
+    void onBegin(System.Action<int> time)
+    {
+        runFloatTime = 0;
+        runIntTime = 0;
+        flag = -1;
+        callback = time;
+    }
+
+    void Update()
+    {
+        TimeTask();
+    }
+
+    /// <summary>
+    /// 设置计时器步长
+    /// </summary>
+    /// <param name="step"></param>
+    public void SetStep(int step) {
+        if (step <= 0)
+        {
+            Debug.LogError("need step > 0");
+            return;
+        }
+        this.step = step;
+    }
+
+    int flag = -1;
+    void TimeTask()
+    {
+        if (isStop) return;
+        runFloatTime += Time.deltaTime;
+        runIntTime = ((int)runFloatTime);
+        if (callback != null && runIntTime % step ==0 && runIntTime != flag)
+        {
+            callback(runIntTime / step);
+            flag = runIntTime;
+        }
+    }
+
+    public void stop() {
+        isStop = true;
+        runFloatTime = 0;
+        runIntTime = 0;
+        flag = -1;
+    }
+
+    public void Clear() {
+        Destroy(gameObject);
+    }
+}
